b15358009b63e0861a1e6ea30de1bf009cb7b3ca
Ruslan Mamedov <rm952576@gmail.com>
2021-11-07 19:00:40 +0300

    добавлен контент для модальных окон

--- js/script.js
+++ js/script.js
@@ -1,33 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
     var signIn = document.querySelector(".sign-in");
+    var signInWrapper = document.querySelector(".sign-in__wrapper")
     var signInBtn = document.querySelector(".header__sign-in-btn");
     var closeSignInBtn = document.querySelector(".sign-in__close");
 
     signInBtn.onclick = function () {
-        signIn.style.display = "block";
+        signInWrapper.style.display = "block";
     }
 
     closeSignInBtn.onclick = function () {
-        signIn.style.display = "none";
+        signInWrapper.style.display = "none";
     }
 
     var signUp = document.querySelector(".sign-up");
+    var signUpWrapper = document.querySelector(".sign-up__wrapper");
     var signUnBtn = document.querySelector(".header__sign-up-btn");
     var closeSignUpBtn = document.querySelector(".sign-up__close");
 
     signUnBtn.onclick = function () {
-        signUp.style.display = "block";
+        signUpWrapper.style.display = "block";
     }
 
     closeSignUpBtn.onclick = function () {
-        signUp.style.display = "none";
+        signUpWrapper.style.display = "none";
     }
 
     window.onclick = function (event) {
         if (event.target == signIn) {
-            signIn.style.display = "none";
+            signInWrapper.style.display = "none";
         } else if (event.target == signUp) {
-            signUp.style.display = "none";
+            signUpWrapper.style.display = "none";
         }
 
     }
